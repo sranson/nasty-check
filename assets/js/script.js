401033ff0675f8df75736caa92a30ee22c3c8ad7
Salome Ranson <salomeranson@gmail.com>
2021-03-27 11:51:24 -0400

    successful api call to cocktailDB - returning Drink IDs

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,15 +33,53 @@ var getMealReceipes = function (mealType, proteinType, cusineType) {
 function formatMealData(data) {
   mealData = data;
   recipeNumber = mealData.hits.length;
-  console.log(recipeNumber);
+  //console.log("Number of Food Recipes:" + recipeNumber);
   for (i = 0; i < recipeNumber; i++) {
-    console.log(mealData.hits[i].recipe.label); // Recipe Label
-    console.log(mealData.hits[i].recipe.source); // Source Name
-    console.log(mealData.hits[i].recipe.image); // Recipe image
-    console.log(mealData.hits[i].recipe.url); // Recipe Instructions URL
+    recipeLabel = mealData.hits[i].recipe.label; // Recipe Label
+    recipeSourceName = mealData.hits[i].recipe.source; // Source Name
+    recipeImage = mealData.hits[i].recipe.image; // Recipe image
+    recipeInstructionsLink = mealData.hits[i].recipe.url; // Recipe Instructions URL
   }
 }
 
 getMealReceipes(mealType, proteinType, cusineType);
 
-// Make API call to CocktailDB to get
+// Make API call to CocktailDB to get the "drink ID" based on "Alcohol ingredient"
+// Will need to pass in a "alcohol type""
+
+let alcoholType = "gin";
+
+var getCocktailID = function (alcoholType) {
+  var cocktailAPI1 =
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + alcoholType;
+
+  fetch(cocktailAPI1)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          getDrinkID(data);
+        });
+      } else {
+        alert("Error" + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to Meal API");
+    });
+};
+
+getCocktailID(alcoholType);
+
+//Grab drink ID
+function getDrinkID(data) {
+  cocktailIDdata = data;
+  cocktailNumber = cocktailIDdata.drinks.length;
+  console.log("Number of Cocktail recipes returned: " + cocktailNumber);
+
+  for (i = 0; i < cocktailNumber; i++) {
+    cocktailID = cocktailIDdata.drinks[i].idDrink;
+    console.log(cocktailID);
+  }
+}
+
+// Make API call to get Cocktail recipes based on the "drink ID"

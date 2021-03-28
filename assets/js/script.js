0bdb408a262552a66f0594d19f765e572018ea39
Salome Ranson <salomeranson@gmail.com>
2021-03-27 20:20:51 -0400

    successfully returning cocktail ingredients, measurements, and instructions based on DrinkID

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,6 @@ function formatMealData(data) {
     recipeSourceName = mealData.hits[i].recipe.source; // Source Name
     recipeImage = mealData.hits[i].recipe.image; // Recipe image
     recipeInstructionsLink = mealData.hits[i].recipe.url; // Recipe Instructions URL
-
     //console.log("Recipe Label " + i + ": " + recipeLabel);
     //console.log("Recipe Source " + i + ": " + recipeSourceName);
     //console.log("Recipe Image " + i + ": " + recipeImage);
@@ -51,15 +50,13 @@ function formatMealData(data) {
   }
 }
 
-// Make API call to CocktailDB to get the "drink ID" based on "Alcohol ingredient"
-// Will need to pass in a "alcohol type""
 
+// Step 1: User provides alcohol type
 let alcoholType = "rum";
 
 
 
-// GET ALCOHOL TYPE FROM USER AND MAKES API CALL
-// PASSES ALL THE DATA FOR SELECTED ALCOHOL TYPE TO THE "getDrinkID" function
+//Step 2: We make an API call based on alcohol type     ====================  PASSES ALL THE DATA FOR SELECTED ALCOHOL TYPE TO THE "formatDrinkData" function  ==================
 var getCocktailAPIdata = function (alcoholType) {
   var cocktailAPI1 =
     "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + alcoholType;
@@ -80,38 +77,91 @@ var getCocktailAPIdata = function (alcoholType) {
 };
 
 
-drinkIDarray = [];
+
+cocktailNameArray = [];
+cocktailImageArray = [];
+cocktailIdArray = []
 
 
-// Data is being passed to this function that includes (1)DRINK NAME, (2)DRINK IMAGE, AND (3)DRINK ID
+// Step 3: FIRST API call returns (1)DRINK NAME (2)DRINK IMAGE (3)DRINK ID into ARRAYS  ================= USE THIS DATA TO SHOW THE USER 5 DRINK CARDS/ OPTIONS ==============================
 function formatDrinkData(data) {
   cockTailData = data;
-  console.log("Number of recipes returned from API call: " + cockTailData.drinks.length);
+  //console.log("Number of recipes returned from API call: " + cockTailData.drinks.length);
 
-  for (i=0; i < 6; i++) {
+  for (i=0; i < 5; i++) {
     cocktailName = cockTailData.drinks[i].strDrink;
     cocktailImage = cockTailData.drinks[i].strDrinkThumb;
     cocktailID = cockTailData.drinks[i].idDrink;
-    getCocktailRecipe(cocktailID);
-    storeAllCocktailVariables(cocktailName, cocktailImage, cocktailID)
+    cocktailNameArray.push(cocktailName);
+    cocktailImageArray.push(cocktailImage);
+    cocktailIdArray.push(cocktailID);
   }
 }
 
 
-function getCocktailRecipe(cocktailID) {
-  drinkIDarray.push(cocktailID);
-  //console.log(drinkIDarray);
+// Add an event listener to ALL 5 Cards ... Based on "e.target", grab the drinkID and store it in a variable -- "drinkID") 
+// THIS IS HARD-CODED FOR NOW
+getCocktailRecipeData("14364");
 
-  for (i=0; i < drinkIDarray.length; i++) {
-      // Make an API call that returns the (1) Ingredients and (2) the instructions;  
-      // Note: the "formatDrinkData" function already has the (1) Cocktail name and (2) Cocktail Image stored in variables
-  } 
-}
 
-function storeAllCocktailVariables(cocktailName, cocktailImage, cocktailID) {
+function getCocktailRecipeData (drinkID) {
+  var cocktailAPI2 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="+drinkID;
+
+  fetch(cocktailAPI2)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          formatCocktailRecipeData(data);
+        });
+      } else {
+        alert("Error" + response.statusText);
+      }
+    })
+    .catch(function (error) {
+      alert("Unable to connect to Meal API");
+    });
+};
+
+
+ingredients = [];
+measurements = [];
+
+function formatCocktailRecipeData(data) {
+  cocktailRecipe = data;
+  //console.log(cocktailRecipe);
+  cocktailName = cocktailRecipe.drinks[0].strDrink;
+  cocktailImage = cocktailRecipe.drinks[0].strDrinkThumb;
+   if (cocktailRecipe.drinks[0].strIngredient1 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient1)};
+   if (cocktailRecipe.drinks[0].strIngredient2 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient2)};
+   if (cocktailRecipe.drinks[0].strIngredient3 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient3)};
+   if (cocktailRecipe.drinks[0].strIngredient4 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient4)};
+   if (cocktailRecipe.drinks[0].strIngredient5 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient5)};
+   if (cocktailRecipe.drinks[0].strIngredient6 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient6)};
+   if (cocktailRecipe.drinks[0].strIngredient7 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient7)};
+   if (cocktailRecipe.drinks[0].strIngredient8 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient8)};
+   if (cocktailRecipe.drinks[0].strIngredient9 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient9)};
+   if (cocktailRecipe.drinks[0].strIngredient10 !== null && cocktailRecipe.drinks[0].strIngredient1 !== "") {ingredients.push(cocktailRecipe.drinks[0].strIngredient10)};
+  
+
+   if (data.drinks[0].strMeasure1 !== null) {measurements.push(data.drinks[0].strMeasure1)};
+   if (data.drinks[0].strMeasure2 !== null) {measurements.push(data.drinks[0].strMeasure2)};
+   if (data.drinks[0].strMeasure3 !== null) {measurements.push(data.drinks[0].strMeasure3)};
+   if (data.drinks[0].strMeasure4 !== null) {measurements.push(data.drinks[0].strMeasure4)};
+   if (data.drinks[0].strMeasure5 !== null) {measurements.push(data.drinks[0].strMeasure5)};
+   if (data.drinks[0].strMeasure6 !== null) {measurements.push(data.drinks[0].strMeasure6)};
+   if (data.drinks[0].strMeasure7 !== null) {measurements.push(data.drinks[0].strMeasure7)};
+   if (data.drinks[0].strMeasure8 !== null) { measurements.push(data.drinks[0].strMeasure8)};
+   if (data.drinks[0].strMeasure9 !== null) {measurements.push(data.drinks[0].strMeasure9)};
+   if (data.drinks[0].strMeasure10 !== null) {measurements.push(data.drinks[0].strMeasure10)};
+
+  instructions = data.drinks[0].strInstructions;
+
+  //return (cocktailName, cocktailImage, ingredients, measurements, instructions)
   console.log(cocktailName);
   console.log(cocktailImage);
-  console.log(cocktailID);
+  console.log(ingredients);
+  console.log(measurements);
+  console.log(instructions);
 }
 
 
@@ -121,3 +171,8 @@ function storeAllCocktailVariables(cocktailName, cocktailImage, cocktailID) {
 getMealReceipes(mealType, proteinType, cusineType);
 getCocktailAPIdata(alcoholType);
 //================================================================================
+
+
+
+
+
